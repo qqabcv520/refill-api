558cc1dbbf96ab39ad7d8b622dd6ba78eaae4675
米饭 <605655316@qq.com>
2017-12-13 22:40:27 +0800

    Initial commit

---
+++ src/main/java/cn/mifan123/refill/RefillApplication.java
@@ -0,0 +1,12 @@
+package cn.mifan123.refill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefillApplication.class, args);
+	}
+}
